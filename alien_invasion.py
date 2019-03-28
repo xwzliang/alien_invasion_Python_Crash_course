4cf67de2261ea23ac9b5f1073676835bc7387ade
broliang <18706703437@163.com>
2019-03-28 18:02:34 +0800

    Draw the ship to the bottom of screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize game settings, and screen object
@@ -8,6 +9,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
@@ -18,6 +22,7 @@ def run_game():
 
         # redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
