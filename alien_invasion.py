fb10e89f8e25af20b72e813426f158899ff17770
broliang <18706703437@163.com>
2019-04-06 22:00:08 +0800

    Creating a row of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,6 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -14,20 +13,19 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship
+    # Make a ship, a group of bullets, and a group of aliens
     ship = Ship(ai_settings, screen)
-
-    # Make a group to store bullets in
     bullets = Group()
+    aliens = Group()
 
-    # Make an alien
-    alien = Alien(ai_settings, screen)
+    # Create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
