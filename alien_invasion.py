ba45548be2440a726b74b7bee1b0aa8727a4f688
broliang <18706703437@163.com>
2019-04-13 21:55:06 +0800

    Drawing the button to the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -13,6 +14,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game statistics
     stats = GameStats(ai_settings)
@@ -34,6 +38,6 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings,stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen,stats, ship, aliens, bullets, play_button)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -38,7 +38,7 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen,stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen"""
     # redraw the screen during each pass through the loop
     screen.fill(ai_settings.bg_color)
@@ -47,6 +47,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the play button if the game is inactive
+    if not stats.game_active:
+        play_button.draw_button()
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
