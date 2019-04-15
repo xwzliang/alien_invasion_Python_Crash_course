b032e62e7be077f7c8b54461878245c929c35330
broliang <18706703437@163.com>
2019-04-15 22:19:09 +0800

    Hiding the mouse when game started and making it visible again when game is ended

--- game_functions.py
+++ game_functions.py
@@ -45,6 +45,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Start a new game when the player clicks Play"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Hide the mouse cursor
+        pygame.mouse.set_visible(False)
+
         # Reset the game statistics
         stats.reset_stats()
         stats.game_active = True
@@ -160,6 +163,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if any aliens have reachec the bottom of the screen"""
